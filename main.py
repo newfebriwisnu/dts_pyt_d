dc8de618b48102eafd167a46af380677d5cfe5a6
newfebriwisnu <newfebriwisnu@gmail.com>
2021-04-13 12:58:30 +0700

    update some filter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import sep
 import pandas as pd
 
 
@@ -6,20 +7,52 @@ def get_data():
     return df
 
 
+def get_column(df=None):
+    result = df.keys().tolist()
+    result.pop(0)
+
+    return result
+
+
 def filter_data(df=None, start_date=None, end_date=None, province=None):
-    result = df[(df['DATE'] >= str(start_date)) &
-                (df['DATE'] <= str(end_date))]
+    if start_date != None and end_date != None:
+        result = df[(df['DATE'] >= str(start_date)) &
+                    (df['DATE'] <= str(end_date))]
 
     if province != None:
-        result = result[['DATE', f'{province}']]
+        result = result[province]
 
     return result.to_string(index=False)
 
 
-df = get_data()
+def main():
+    df = get_data()
 
-result = filter_data(df=df,
-                     start_date='2021-01-01',
-                     end_date='2021-01-10',
-                     province='DIY')
-print(result)
+    column_get = get_column(df)
+    selected_column = ['DATE']
+
+    print()
+    print('Data Covid Indonesia by @kawalcovid19\n')
+    print('\tList Kota')
+    print(*column_get, sep='\t ')
+    print()
+
+    total_kota = int(input('Total Kota: '))
+    for x in range(0, total_kota):
+        selected_column.append(input(f'Kota {x+1}: ').upper())
+    print()
+
+    start_date = input('Start Date (YYYY-MM-DD): ')
+    end_date = input('End Date (YYYY-MM-DD): ')
+    print()
+
+    result = filter_data(df=df,
+                         start_date=start_date,
+                         end_date=end_date,
+                         province=selected_column)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
